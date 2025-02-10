9a7d4a6e490a379f3691f1b48f9eec7373e48aac
影入平羌 <hwenwur@gmail.com>
2025-02-10 18:34:57 +0800

    Update bilibili.js

--- tampermonkey/bilibili.js
+++ tampermonkey/bilibili.js
@@ -2,9 +2,10 @@
 // @name         Bilibili净化
 // @namespace    http://tampermonkey.net/
 // @version      2025-02-09
-// @description  高斯模糊图片，隐藏热搜
+// @description  try to take over the world!
 // @author       You
 // @match        https://www.bilibili.com/*
+// @match        https://t.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
@@ -12,7 +13,8 @@
 (function() {
     'use strict';
     // 你要模糊的 div 的选择器
-    const unwantedDivSelectors = ['img', '.trending'];
+    const blurSelectors = ['.bili-dyn-card-video__stat__item', '.view', '.playinfo', '.carousel-inner__img', '.cover', '.bili-video-card__cover', '.bpx-player-ending-related', '.pic-box', '.bili-video-card__stats--left'];
+    const hideSelectors = ['.loc-mc-box', '.bili-dyn-topic-box', '.bili-dyn-live-users__item__living', '.bili-dyn-banner', '.trending'];
 
     // 模糊处理函数
     function blurDivs(selector) {
@@ -21,10 +23,19 @@
             div.style.filter = 'blur(5px)';
         });
     }
+    function hideDivs(selector) {
+        const divs = document.querySelectorAll(selector);
+        divs.forEach(div => {
+            div.style.display = 'none';
+        });
+    }
 
     // 初始模糊处理
-    unwantedDivSelectors.forEach((s) => {
+    blurSelectors.forEach((s) => {
         blurDivs(s);
+    });
+    hideSelectors.forEach((s) => {
+        hideDivs(s);
     });
 
     // 监听 DOM 变化并处理新加载的内容
@@ -35,8 +46,11 @@
 
         mutations.forEach((mutation) => {
             if (mutation.addedNodes.length) {
-                unwantedDivSelectors.forEach((s) => {
+                blurSelectors.forEach((s) => {
                     blurDivs(s);
+                });
+                hideSelectors.forEach((s) => {
+                    hideDivs(s);
                 });
             }
         });
